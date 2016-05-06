d2cedbdbc9412b7a357edb3f4892ab00af0a05b0
Benjamin Kott <benjamin.kott@wfp2.com>
2016-05-06 04:00:16 +0200

    [TASK] Set timezone

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -32,6 +32,12 @@ use Symfony\Component\Config\Loader\LoaderInterface;
  */
 class AppKernel extends Kernel
 {
+    public function __construct($environment, $debug)
+    {
+        date_default_timezone_set('Europe/Berlin');
+        parent::__construct($environment, $debug);
+    }
+
     public function registerBundles()
     {
         $bundles = [
