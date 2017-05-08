6dd9b34d5d581e3df9b40b4b1319dc87443bea00
Benjamin Kott <benjamin.kott@wfp2.com>
2017-05-08 23:51:06 +0200

    [TASK] Optimize string conversion

--- src/BK2K/Sitepackage/GeneratorBundle/Utility/StringUtility.php
+++ src/BK2K/Sitepackage/GeneratorBundle/Utility/StringUtility.php
@@ -49,6 +49,7 @@ class StringUtility
         $string = self::clean($string);
         $string = preg_replace('/[^a-z0-9]+/i', ' ', $string);
         $string = trim($string);
+        $string = strtolower($string);
         $string = ucwords($string);
         $string = str_replace(' ', '', $string);
         return $string;
@@ -60,7 +61,16 @@ class StringUtility
      */
     public static function camelCaseToLowerCaseUnderscored($string)
     {
-        return strtolower(preg_replace('/(?<=\\w)([A-Z])/', '_\\1', self::clean($string)));
+        preg_match_all(
+            '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!',
+            self::clean($string),
+            $matches
+        );
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('_', $ret);
     }
 
     /**
@@ -69,6 +79,15 @@ class StringUtility
      */
     public static function camelCaseToLowerCaseDashed($string)
     {
-        return strtolower(preg_replace('/(?<=\\w)([A-Z])/', '-\\1', self::clean($string)));
+        preg_match_all(
+            '!([A-Z][A-Z0-9]*(?=$|[A-Z][a-z0-9])|[A-Za-z][a-z0-9]+)!',
+            self::clean($string),
+            $matches
+        );
+        $ret = $matches[0];
+        foreach ($ret as &$match) {
+            $match = $match == strtoupper($match) ? strtolower($match) : lcfirst($match);
+        }
+        return implode('-', $ret);
     }
 }
