62d6b7ecc67a7cc7598ae3c206ef77f8ed88acfc
Benjamin Kott <benjamin.kott@outlook.com>
2024-10-17 13:20:36 +0200

    [TASK] Replace twitter with x

--- src/Twig/Extension/GlobalVariablesExtension.php
+++ src/Twig/Extension/GlobalVariablesExtension.php
@@ -23,7 +23,7 @@ class GlobalVariablesExtension extends AbstractExtension implements GlobalsInter
                     'name' => 'Benjamin Kott',
                     'email' => 'benjamin.kott@outlook.com',
                     'hash' => md5('benjamin.kott@outlook.com'),
-                    'twitter' => 'benjaminkott',
+                    'x' => 'benjaminkott',
                     'github' => 'benjaminkott',
                     'description' => 'Benjamin is Frontend-Developer at <a class="font-weight-bold" href="https://www.typo3.com" target="_blank">TYPO3</a> and worked on projects based on TYPO3 CMS from mid- to enterprise size. Since 2014 his <a class="font-weight-bold" href="https://github.com/benjaminkott/bootstrap_package" target="_blank">Bootstrap Package</a> is used as codebase for the official TYPO3 CMS Introduction Package with the goal to provide an extensive best practice example on how to create websites efficiently with TYPO3 CMS.',
                 ],
